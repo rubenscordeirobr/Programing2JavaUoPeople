d8c546e85d0b625af26c947216ffea4a64832aa8
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-11-23 20:15:53 +0100

    Add EnumDescription interface to provide name and description for enum constants

---
+++ unit-02/assignment/src/com/ecommerce/enums/EnumDescription.java
@@ -0,0 +1,28 @@
+package com.ecommerce.enums;
+
+/**
+ * The EnumDescription interface defines a blueprint for enumerations that
+ * provide both a name and a description for each enum constant.
+ * <p>
+ * This interface is intended to enhance enumerations by adding descriptive
+ * information, which can be useful for displaying meaningful data to users.
+ * It can be implemented by any enum type that requires a name and description
+ * to represent its constants.
+ * </p>
+ */
+public interface EnumDescription {
+
+    /**
+     * Retrieves the name of the enum constant.
+     *
+     * @return a String representing the name of the enum constant.
+     */
+    String getName();
+
+    /**
+     * Retrieves a description of the enum constant.
+     *
+     * @return a String representing the description of the enum constant.
+     */
+    String getDescription();
+}
