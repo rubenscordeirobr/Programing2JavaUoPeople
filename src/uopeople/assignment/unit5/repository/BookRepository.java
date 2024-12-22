eb6672ebf467923988569b5ac16b507705c62ffc
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-22 18:57:47 +0100

    Add BookRepository class to manage Book entities with basic CRUD operations

---
+++ src/uopeople/assignment/unit5/repository/BookRepository.java
@@ -0,0 +1,35 @@
+package uopeople.assignment.unit5.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import uopeople.assignment.unit5.models.aggregators.Book;;
+
+public class BookRepository extends RepositoryBase<Book> {
+
+    @Override
+    public Optional<Book> get(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public List<Book> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public void save(Book entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void delete(Book entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+ 
+    
+}
