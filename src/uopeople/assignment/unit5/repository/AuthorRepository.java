ad6fb2d6f558828b794ee0acb544b2a26f313472
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-22 18:57:32 +0100

    Add AuthorRepository class to manage Author entities with basic CRUD operations

---
+++ src/uopeople/assignment/unit5/repository/AuthorRepository.java
@@ -0,0 +1,36 @@
+package uopeople.assignment.unit5.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import uopeople.assignment.unit5.models.Author;
+
+public class AuthorRepository extends RepositoryBase<Author> {
+
+    @Override
+    public Optional<Author> get(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public List<Author> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public void save(Author entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void delete(Author entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    
+    
+}
