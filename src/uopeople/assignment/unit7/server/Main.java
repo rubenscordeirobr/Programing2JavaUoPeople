c5e7d812ebf3e16b58b4caa2952b9d5c482dfb38
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-29 17:40:43 +0100

    Add Main class to initialize and start the Chat Server application

---
+++ src/uopeople/assignment/unit7/server/Main.java
@@ -0,0 +1,61 @@
+/*
+ * Project: UoPeople Chat Application (Unit 7 Assignment)
+ * Description: This is a chat server-client application designed to demonstrate networking, threading, and Java concepts.
+ * Author: Rubens Cordeiro
+ */
+
+ package uopeople.assignment.unit7.server;
+
+ import uopeople.assignment.unit7.shared.ChatConfigurations;
+import uopeople.assignment.unit7.shared.LogEvent;
+import uopeople.assignment.unit7.utils.*;
+ 
+ /**
+  * Main entry point for the Chat Server application.
+  * This class initializes and starts the server, and handles log events.
+  */
+ public class Main {
+ 
+     /** Singleton instance of the ChatServer. */
+     private static final ChatServer chatServer = new ChatServer(ChatConfigurations.PORT);
+ 
+     /**
+      * Main method to launch the Chat Server.
+      *
+      * @param args Command-line arguments (not used).
+      */
+     public static void main(String[] args) {
+         chatServer.setLogEventConsumer(Main::log);
+         chatServer.startServer();
+     }
+ 
+     /**
+      * Handles logging of events with different log levels.
+      *
+      * @param logEvent The event to be logged.
+      */
+     public static void log(LogEvent logEvent) {
+         switch (logEvent.getTypeLog()) {
+ 
+             case INFO:
+                 PrintUtil.printMessage(logEvent.getFullMessage());
+                 break;
+ 
+             case SUCCESS:
+                 PrintUtil.printSuccessMessage(logEvent.getFullMessage());
+                 break;
+ 
+             case WARNING:
+                 PrintUtil.printWarningMessage(logEvent.getFullMessage());
+                 break;
+ 
+             case ERROR:
+                 PrintUtil.printFailMessage(logEvent.getFullMessage());
+                 break;
+ 
+             default:
+                 throw new IllegalArgumentException("Event type not supported: " + logEvent.getTypeLog());
+         }
+     }
+ }
+ 
