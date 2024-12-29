780e6d6ac85a05ca8f1a4b0f3d796d3de24227bd
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-29 17:42:39 +0100

    Add EnumDescription interface for enum constant name and description retrieval

---
+++ src/uopeople/assignment/unit7/utils/EnumDescription.java
@@ -0,0 +1,18 @@
+package uopeople.assignment.unit7.utils;
+
+public interface EnumDescription {
+
+    /**
+     * Retrieves the name of the enum constant.
+     *
+     * @return a String representing the name of the enum constant.
+     */
+    String getName();
+
+    /**
+     * Retrieves a description of the enum constant.
+     *
+     * @return a String representing the description of the enum constant.
+     */
+    String getDescription();
+}
