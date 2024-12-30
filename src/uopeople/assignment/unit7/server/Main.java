d8a9e6343dbdbf4db280591c888d6de8d2b94443
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-30 12:31:50 +0100

    Enhance logging in Chat Server by formatting messages with the current time

--- src/uopeople/assignment/unit7/server/Main.java
+++ src/uopeople/assignment/unit7/server/Main.java
@@ -4,58 +4,59 @@
  * Author: Rubens Cordeiro
  */
 
- package uopeople.assignment.unit7.server;
+package uopeople.assignment.unit7.server;
 
- import uopeople.assignment.unit7.shared.ChatConfigurations;
+import uopeople.assignment.unit7.shared.ChatConfigurations;
 import uopeople.assignment.unit7.shared.LogEvent;
 import uopeople.assignment.unit7.utils.*;
- 
- /**
-  * Main entry point for the Chat Server application.
-  * This class initializes and starts the server, and handles log events.
-  */
- public class Main {
- 
-     /** Singleton instance of the ChatServer. */
-     private static final ChatServer chatServer = new ChatServer(ChatConfigurations.PORT);
- 
-     /**
-      * Main method to launch the Chat Server.
-      *
-      * @param args Command-line arguments (not used).
-      */
-     public static void main(String[] args) {
-         chatServer.setLogEventConsumer(Main::log);
-         chatServer.startServer();
-     }
- 
-     /**
-      * Handles logging of events with different log levels.
-      *
-      * @param logEvent The event to be logged.
-      */
-     public static void log(LogEvent logEvent) {
-         switch (logEvent.getTypeLog()) {
- 
-             case INFO:
-                 PrintUtil.printMessage(logEvent.getFullMessage());
-                 break;
- 
-             case SUCCESS:
-                 PrintUtil.printSuccessMessage(logEvent.getFullMessage());
-                 break;
- 
-             case WARNING:
-                 PrintUtil.printWarningMessage(logEvent.getFullMessage());
-                 break;
- 
-             case ERROR:
-                 PrintUtil.printFailMessage(logEvent.getFullMessage());
-                 break;
- 
-             default:
-                 throw new IllegalArgumentException("Event type not supported: " + logEvent.getTypeLog());
-         }
-     }
- }
- 
+
+/**
+ * Main entry point for the Chat Server application.
+ * This class initializes and starts the server, and handles log events.
+ */
+public class Main {
+
+    /** Singleton instance of the ChatServer. */
+    private static final ChatServer chatServer = new ChatServer(ChatConfigurations.PORT);
+
+    /**
+     * Main method to launch the Chat Server.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+        chatServer.setLogEventConsumer(Main::log);
+        chatServer.startServer();
+    }
+
+    /**
+     * Handles logging of events with different log levels.
+     *
+     * @param logEvent The event to be logged.
+     */
+    public static void log(LogEvent logEvent) {
+
+        String message = ChatUtil.formatCurrentTime(logEvent.getFullMessage());
+        switch (logEvent.getTypeLog()) {
+
+            case INFO:
+                PrintUtil.printMessage(message);
+                break;
+
+            case SUCCESS:
+                PrintUtil.printSuccessMessage(message);
+                break;
+
+            case WARNING:
+                PrintUtil.printWarningMessage(message);
+                break;
+
+            case ERROR:
+                PrintUtil.printFailMessage(message);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Event type not supported: " + logEvent.getTypeLog());
+        }
+    }
+}
