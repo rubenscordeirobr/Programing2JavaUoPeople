ad347acdff3b3a599a935fc9cd0ebdb4782d1a40
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-30 12:28:50 +0100

    Add method to format messages with the current time in Util class

--- src/uopeople/assignment/unit7/utils/Util.java
+++ src/uopeople/assignment/unit7/utils/Util.java
@@ -1,22 +1,37 @@
- package uopeople.assignment.unit7.utils;
+package uopeople.assignment.unit7.utils;
 
- /**
-  * Utility class containing helper methods for general-purpose operations.
-  */
- public class Util {
- 
-     /**
-      * Attempts to parse an integer from a given string.
-      *
-      * @param string The input string to parse.
-      * @return The parsed integer, or 0 if the input string is not a valid integer.
-      */
-     public static int tryParseInt(String string) {
-         try {
-             return Integer.parseInt(string);
-         } catch (NumberFormatException e) {
-             return 0;
-         }
-     }
- }
- 
+import java.time.LocalTime;
+
+/**
+ * Utility class containing helper methods for general-purpose operations.
+ */
+public class Util {
+
+    /**
+     * Attempts to parse an integer from a given string.
+     *
+     * @param string The input string to parse.
+     * @return The parsed integer, or 0 if the input string is not a valid integer.
+     */
+    public static int tryParseInt(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    /**
+     * Formats a message with the current time.
+     *
+     * @param message The message to format.
+     * @return The formatted message.
+     */
+    public static String getCurrentTime() {
+        LocalTime date = LocalTime.now();
+        return String.format("%02d:%02d:%02d",
+                date.getHour(),
+                date.getMinute(),
+                date.getSecond());
+    }
+}
