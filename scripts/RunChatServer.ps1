26c3ea51bd508c077bd1d12e1e34bbd99949755a
Rubens Cordeiro <rubens.cordeiro@live.com>
2024-12-30 12:32:34 +0100

    Update output path and source directory in RunChatServer script for relative path adjustments

--- scripts/RunChatServer.ps1
+++ scripts/RunChatServer.ps1
@@ -3,8 +3,8 @@
 
 # Parameters for the server application
 $mainClass = "uopeople.assignment.unit7.server.Main"
-$outputPath = "./bin"
-$sourceDir = "./src/uopeople/assignment/unit7"
+$outputPath = "../bin"
+$sourceDir = "../src/uopeople/assignment/unit7"
 
 # Run the ChatServer
 Run-JavaApp -MainClass $mainClass -OutputPath $outputPath -SourceDir $sourceDir
